ac758418d4b8ed9b6288c463ffd6df438f9fd40f
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2023-10-27 21:04:00 -0300

    Estudo sobre:
    print() e introdução a tipos de dados

---
+++ aula-02.py
@@ -0,0 +1,6 @@
+# \r\n -> CRLF
+# \n -> LF
+print(12, 34, 189, sep='-', end=' / ')
+print(70, 90, sep='-', end=' / ')
+print(7, 100, sep='-', end=' / ')
+print()
---
+++ aula-03.py
@@ -0,0 +1,17 @@
+# Strings são textos que estão dentro de aspas
+
+print(1234)
+
+# Aspas simples
+print('Hello, World!')
+print(1, 'Hello, World! "Olá, Mundo!"')
+
+# Aspas duplas
+print("Hello, World!")
+print(2, "Hello, World! 'Olá, Mundo!'")
+
+# Escape
+print('Hello, World! \"Olá, Mundo!\"')
+
+# r
+print(r'Hello, World! \"Olá, Mundo!\"')
