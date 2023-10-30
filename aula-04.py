986d17dee80a6e6ce760b2bc13bba48697b071cb
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2023-10-30 13:22:17 -0300

    Aulas sobre tipos de dados

---
+++ aula-04.py
@@ -0,0 +1,21 @@
+# Tipos de dados
+
+# int -> Número inteiro
+# O tipo imt representa qualquer número
+# positivo  ou negativo. int sem sinal é considerado
+# positivo
+print(12) # int
+print(-12) # int
+print(0) # int
+
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante.
+# float sem sinal é considerado positivo.
+print(12.5) # float
+print(-12.5)
+print(0.0)
+
+# A função type mostra o tipo que o Python
+# inferiu ao valor
+print(type(17))
+print(type(17.5))
