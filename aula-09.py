0691ad6337034d09fea19d5ecbff638b427ffcfe
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2023-11-07 20:47:31 -0300

    continuação sobre operadores aritiméticos

--- aula-09.py
+++ aula-09.py
@@ -13,3 +13,8 @@ print('Divisão', divisao)
 divisao_inteira = 10 // 2.2
 print('Divisão inteira', divisao_inteira)
 
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2
+print('Módulo', modulo )
