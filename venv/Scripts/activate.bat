d0a833a1268ba0fd7959b74f4ddb8b33cf0b8240
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-01-01 17:49:00 -0300

    Aula 5 sobre input() e organização do setup

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\caua1\OneDrive\Documents\Cursos de programação\python\venv
+set VIRTUAL_ENV=C:\Users\Cauã\OneDrive\Documents\cursos_de_programacao\python\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
