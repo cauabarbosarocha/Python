dd07b83e14fbce53f67abdea3e7e7f25002714a0
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-01-02 21:40:37 -0300

    Aula 18 - Operadores de comparação

---
+++ aula-18.py
@@ -0,0 +1,20 @@
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou iqual      2 >= 2
+<       menor               1 < 2
+<=      menor ou iqual      1 <= 1
+==      iqual               'a' = 'a'
+!=      diferente           'a' != 'b'
+"""
+
+maior = 2 > 1
+maior_ou_iqual = 2 >=2
+menor = 1 < 2
+menor_ou_iqual = 1 < 1
+iqual = 'a' == 'a'
+diferente = 'a' != 'b'
+print(
+    
+)
