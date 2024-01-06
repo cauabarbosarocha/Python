b5147360171ca70cdbc8d0866db69c04db5a733e
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-01-05 22:33:49 -0300

    Aula - 22 | Sobre o operador lógico not

--- aula-22.py
+++ aula-22.py
@@ -1,20 +1,8 @@
 # Operador Lógico
 # and (e) or (ou) not (não)
-# 
-# 
-# 
-# 
-# São considerados falsy (que você já viu)
-# 0 0.0 '' False
-# Também existe o tipo None que é
-# usando para representar em não valor
+# Usado para inverter expressões
+# not True = False
+# not False = True
 
-entrada = input('[E] Entrar [S] Sair:  ')
-senha_digitada = input('Digite sua senha:  ')
-senha_permitida = '1234'
-if entrada == 'E' or entrada == 'e' and senha_permitida == senha_digitada:
-    print('Entrar')
-elif entrada == 'S' or entrada == 's' and senha_permitida == senha_digitada:
-    print('Sair')
-else:
-    print('Acesso negado')
+print(not True)
+print(not False)
