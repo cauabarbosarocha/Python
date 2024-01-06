6570895c5ae493b1c3663b824fcb3139b7058666
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-01-05 22:23:53 -0300

    Aula - 21 | Continuação sobre o operador lógico or

--- aula-21.py
+++ aula-21.py
@@ -1,21 +1,23 @@
 # Operador Lógico
 # and (e) or (ou) not (não)
-# or - Qualquel condição verdadeira avalia
+# or - Qualquer condição verdadeira avalia
 # toda a expressão como verdadeira.
 # Se qualquer valor for coniderado verdadeiro,
 # a expressão inteira será avaliada naquele valor.
 # São considerados falsy (que você já viu)
 # 0 0.0 '' False
 # Também existe o tipo None que é
-# usando para representar em não valor
-# 
+# usado para representar em não valor 
 
-entrada = input('[E] Entrar [S] Sair:  ')
-senha_digitada = input('Digite sua senha:  ')
-senha_permitida = '1234'
-if entrada == 'E' or entrada == 'e' and senha_permitida == senha_digitada:
-    print('Entrar')
-elif entrada == 'S' or entrada == 's' and senha_permitida == senha_digitada:
-    print('Sair')
-else:
-    print('Acesso negado')
+# entrada = input('[E] Entrar [S] Sair:  ')
+# senha_digitada = input('Digite sua senha:  ')
+# senha_permitida = '1234'
+# if (entrada == 'E' or entrada == 'e') and senha_permitida == senha_digitada:
+#     print('Entrar')
+# elif (entrada == 'S' or entrada == 's') and senha_permitida == senha_digitada:
+#     print('Sair')
+# else:
+#     print('Acesso negado')
+
+senha = input('Senha: ') or 'Sem senha'
+print(senha)
