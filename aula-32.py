01826b008a6c0176965b3a4adc3f0510b4d59b0a
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-01-16 19:45:54 -0300

    Aula-32 | Tipos built-in, documentação, tipos imutáveis, método de string

---
+++ aula-32.py
@@ -0,0 +1,11 @@
+"""
+TIPOS BUILT-IN, DOCUMENTAÇÃO, TIPO IMUTÁVEL, MÉTODO DE STRING
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis: str, int, float, bool
+"""
+string = 'Cauã Barbosa'
+outra_variavel = f'{string[:4]} ABC {string[5:]}'
+print(string)
+print(outra_variavel)
+print(string.capitalize())
+print(string.zfill(20))
