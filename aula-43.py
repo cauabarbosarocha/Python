189bdb2a788fe8030dc66577e14fcf2ccced4990
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-01-22 20:40:08 -0300

    Aula-43 | Introdução ao for/in (estrutura de repetição finita)

---
+++ aula-43.py
@@ -0,0 +1,15 @@
+# senha_salva = '1606'
+# senha_digitada = ''
+# repeticoes = 0
+
+# while senha_salva != senha_digitada:
+#     senha_digitada = input(f'Digite usa senha ({repeticoes}x): ')
+
+#     repeticoes += 1
+
+# print('Isso poderia ter sido infinito.')
+
+texto = 'Pyhon'
+
+for letra in texto:
+    print(letra)
