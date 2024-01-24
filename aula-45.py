a8083443d075545c054eeabb743cfdf99d5f324b
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-01-24 20:24:09 -0300

    Aula-45 | Como funciona o for/in (part-2)

--- aula-45.py
+++ aula-45.py
@@ -4,7 +4,13 @@ Iterador = quem sabe entregar um valor por vez
 next = me entregue o próximo valor
 iter = me entregue seu iterador
 """
-numeros = range(0, 100, 8)
+text = 'Cauã' # iterável
+# iterador = iter(text) # iterador
 
-for numero in numeros:
-    print(numero)
+# while True:
+#     try:
+#         print(next(iterador))
+#     except StopIteration:
+#         break
+for letra in text:
+    print(letra)
