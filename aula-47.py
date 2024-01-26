a7357efe16e4b59c841ad3b1fa86a38aeac76736
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-01-25 21:31:52 -0300

    Aula-47 | Jogo de palavras

--- aula-47.py
+++ aula-47.py
@@ -10,3 +10,38 @@ letra digitada está na palavra secreta.
     exiba *;
 Faça a contagem de tentativas do seu usuário.
 """
+# import os
+
+palavra_secreta = 'lasanha'
+letras_acertadas = ''
+tentativas = 0
+
+while True:
+    letra_digitada = input('Digite um letra: ')
+    tentativas += 1
+
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra.')
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+    
+    palavra_formada = ''
+
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            palavra_formada +=letra_secreta
+        else:
+            palavra_formada += '*'
+
+    print('Palavra secreta:', palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        # os.system('cls')
+        print('VOCÊ GANHOU!! PARABÉNS')
+        print('A plavra era:', palavra_secreta)
+        print('Tentativas:', tentativas)
+        letras_acertadas = ''
+        tentativas = 0
+    
