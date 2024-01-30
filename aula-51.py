d19ba64811f534194259d04a2f276fd0803e7ca7
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-01-30 20:13:03 -0300

    Aula-51 | Exercício

---
+++ aula-51.py
@@ -0,0 +1,13 @@
+# Exercício
+# Exiba os indices da lista
+lista = ['Lasanha', 'Aparmediana', 'Nhoque']
+indices = range(len(lista))
+
+# indice = 0
+
+# for comida in lista:
+#     print(indice, comida)
+#     indice += 1
+
+for indice in indices:
+    print(indice, lista[indice])
