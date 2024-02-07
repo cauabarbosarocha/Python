915c6be812aec5206e23efe7945c99a67d906ccb
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-02-06 21:17:54 -0300

    Aula-55 | Exercício - minha resposta

---
+++ aula-55.py
@@ -0,0 +1,32 @@
+"""
+Faça uma lista de compras
+O usuário  deve ter a possibilidade de
+inserir, aapgar, e listar valores da sua lista
+Não permita que o programa quebre com erros de índices inexistentes na lista.
+"""
+# import os
+
+lista = []
+
+while True:
+    entrada = input('Selecione uma opção: \n [i]nserir  [a]pagar  [l]istar:    ')
+    lista_enumerada = enumerate(lista)
+    apagar = entrada == 'a' and lista == []
+    listar = entrada == 'l' and lista == []
+
+    if entrada == 'i':
+        valor = input('Item: ')
+        lista.append(valor)
+        continue
+
+    if apagar:
+        indice = input('Escolha um índice: ')
+    # item_apagado = float(indice)
+
+    if listar:
+        print(next(lista_enumerada))
+        continue
+    else:
+        print('Não à nada para listar.')
+        continue
+
