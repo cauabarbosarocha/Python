b626d1b344ea7cac1085f9b7a2d9b9807b207f31
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-02-25 18:09:25 -0300

    Exercício calculo do segundo digito e validação

--- aula-62.py
+++ aula-62.py
@@ -23,13 +23,29 @@ contrário disso:
 
 O primeiro dígito do CPF é 7
 """
-cpf = '74682489070'
-nove_digitos = cpf[:9]
+cpf_usuario = input('Digite seu CPF: ')
+nove_digitos = cpf_usuario[:9]
 contador_regressivo_1 = 10
 
 soma_1 = 0
-for digito_1 in nove_digitos:
-    soma_1 += int(digito_1) * contador_regressivo_1
+for digito in nove_digitos:
+    soma_1 += int(digito) * contador_regressivo_1
     contador_regressivo_1 -= 1
 digito_1 = (soma_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
+
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+soma_2 = 0
+for digito in dez_digitos:
+    soma_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (soma_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+cpf_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+if cpf_usuario == cpf_calculo:
+    print(f'{cpf_usuario} válido')
+else:
+    print('CPF inválido')
