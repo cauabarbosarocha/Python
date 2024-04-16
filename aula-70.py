57908b252fe848e6c5d212901c30480374d3dbae
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-04-16 18:07:51 -0300

    Aula-70 | Exercícios com funções

---
+++ aula-70.py
@@ -0,0 +1,13 @@
+# Exercícios com funções
+
+"""
+Crie uma função que multiplica todos os argumentos
+não nomeados recebidos.
+Retorne o total para uma variável e mostre o valor
+da valiável
+"""
+
+"""
+Crie uma função que fale se um númemro é par ou impar.
+Retorne se o número é par ou impar
+"""
