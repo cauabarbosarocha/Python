8dd6d5668a0f638b4b5848aee4e682cc7a6fb386
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2024-04-19 20:13:00 -0300

    Aula-72 | Closure e funções que retornam outras funções

---
+++ aula-72.py
@@ -0,0 +1,14 @@
+"""
+Closure e funções que retornam outras funções
+"""
+def saudacao(saudacao):
+    def saudar(nome):
+        return f'{saudacao}, {nome}'
+    return saudar
+
+falar_bom_dia = saudacao('Bom dia')
+falar_bom_dia = saudacao('Boa noite')
+
+for nomes in ['Cauã', 'Jorge', 'Tom']:
+    print(falar_bom_dia(nomes))
+    print(falar_bom_dia(nomes))
