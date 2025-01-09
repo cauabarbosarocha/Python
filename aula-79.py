a029c695581ffdda4193ab362c93abccfdbb36aa
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-01-09 19:12:37 -0300

    Peculiaridades do tipo set em Python

--- aula-79.py
+++ aula-79.py
@@ -8,10 +8,11 @@
 # Criando um set
 # set(iterável) ou {1, 2, 3}
 # set_1 = set('Cauã')
-set_1 = set() # vazio
-set_2 = {'Cauã',16, 6, 12} # com dados
-print(set_1, type(set_1))
-print(set_2, type(set_2))
+
+# set_1 = set() # vazio
+# set_2 = {'Cauã',16, 6, 12} # com dados
+# print(set_1, type(set_1))
+# print(set_2, type(set_2))
 
 # Sets são eficientes para remover valores duplicados
 # de iteráveis.
@@ -21,8 +22,19 @@ print(set_2, type(set_2))
 # - não garantem ordem;
 # - são iteráveis (for, in, not in)
 
+# lista_1 = [12, 16, 6, 12, 6, 6, 8, 8, 8, 8]
+# set_3 = set(lista_1)
+# lista_2 = set_3
+# print(lista_2)
+# print(8 in set_3)
+# print(4 not in set_3)
+# for numero in set_3:
+#     print(numero)
+
 # Métodos úteis:
 # add, update, clear, discard
+
+
 
 # Operadores úteis:
 # união | união (union) - Une
