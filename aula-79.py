94790bbbfcdf61de91c8e3a27b1d276d4b7aafbd
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-01-09 19:11:41 -0300

    Aula-79 | Introdução ao tipo de dado "set"

---
+++ aula-79.py
@@ -0,0 +1,31 @@
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos são ensinados na matemática
+# https://brasilescola.uol.com.br/matematica/conjunto.htm
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
+
+# Criando um set
+# set(iterável) ou {1, 2, 3}
+# set_1 = set('Cauã')
+set_1 = set() # vazio
+set_2 = {'Cauã',16, 6, 12} # com dados
+print(set_1, type(set_1))
+print(set_2, type(set_2))
+
+# Sets são eficientes para remover valores duplicados
+# de iteráveis.
+# - Não aceitam valores mutáveis;
+# - Seus valores serão sempre únicos;
+# - não tem índexes;
+# - não garantem ordem;
+# - são iteráveis (for, in, not in)
+
+# Métodos úteis:
+# add, update, clear, discard
+
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
