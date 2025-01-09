b84586857b97b70938a2947b005f4dfa0f5f0b3e
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-01-09 19:27:20 -0300

    Métodos úteis para o tipo set

--- aula-79.py
+++ aula-79.py
@@ -9,10 +9,10 @@
 # set(iterável) ou {1, 2, 3}
 # set_1 = set('Cauã')
 
-# set_1 = set() # vazio
-# set_2 = {'Cauã',16, 6, 12} # com dados
-# print(set_1, type(set_1))
-# print(set_2, type(set_2))
+set_1 = set() # vazio
+set_2 = {'Cauã',16, 6, 12} # com dados
+print(set_1, type(set_1))
+print(set_2, type(set_2))
 
 # Sets são eficientes para remover valores duplicados
 # de iteráveis.
@@ -22,14 +22,14 @@
 # - não garantem ordem;
 # - são iteráveis (for, in, not in)
 
-# lista_1 = [12, 16, 6, 12, 6, 6, 8, 8, 8, 8]
-# set_3 = set(lista_1)
-# lista_2 = set_3
-# print(lista_2)
-# print(8 in set_3)
-# print(4 not in set_3)
-# for numero in set_3:
-#     print(numero)
+lista_1 = [12, 16, 6, 12, 6, 6, 8, 8, 8, 8]
+set_3 = set(lista_1)
+lista_2 = set_3
+print(lista_2)
+print(8 in set_3)
+print(4 not in set_3)
+for numero in set_3:
+    print(numero)
 
 # Métodos úteis:
 # add, update, clear, discard
@@ -48,3 +48,14 @@ print(set_4)
 # intersecção & (intersection) - Itens presentes em ambos
 # diferença - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+
+set_5 = {1, 2, 3}
+set_6 = {2, 3, 4}
+set_7 = set_5 | set_6
+set_8 = set_5 & set_6
+set_9 = set_5 - set_6 # set_6 - set_5 = 4
+set_10 = set_5 ^ set_6
+print(set_7)
+print(set_8)
+print(set_9)
+print(set_10)
