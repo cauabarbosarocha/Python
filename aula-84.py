e329130b88f3bfad758f1e837116e68b596c7ee8
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-01-14 20:42:04 -0300

    Conclusão da aula 84 sobre "args" e "kwargs"

--- aula-84.py
+++ aula-84.py
@@ -3,15 +3,45 @@ a, b = 1, 2
 a, b = b, a
 # print(a, b)
 
+
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b1, b2)
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
 pessoa = {
     'nome': 'Heloisa',
     'sobrenome': 'Martins'
 }
 
-(a1, a2), (b1, b2) = pessoa.items()
-print(a1, a2)
-print(b1, b2)
+dados_pessoa = {
+    'idade': 18,
+    'altura': 1.58
+}
+
+pessoas_completas = {**pessoa, **dados_pessoa}
+# print(pessoas_completas)
 
 # args e kwargs
 # args (já vi)
 # kwargs - keyword arguments (argumentos nomeados)
+
+def mostra_argumentos_nomeados(*args, **kwargs):
+    print('NÃO NOMWADOS:', args)
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+
+# mostra_argumentos_nomeados(nome='Cauã', qlqr=123)
+
+# mostra_argumentos_nomeados(**pessoas_completas)
+
+confgs = {
+    'args1': 1,
+    'args2': 2,
+    'args3': 3,
+    'args4': 4,
+}
+
+mostra_argumentos_nomeados(**confgs)
