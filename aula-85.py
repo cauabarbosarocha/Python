85779ca39bd792b51e21eba12b1ed80fb63b58a2
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-01-14 21:07:19 -0300

    Mapeamento de dados em list comprehension (é possível sim criar listas complexas

--- aula-85.py
+++ aula-85.py
@@ -9,4 +9,19 @@ for numero in range(10):
 # print(lista)
 
 lista = [numero*2 for numero in range(10)]
-print(lista)
+# print(list(range(10)))
+# print(lista)
+
+# Mapeamento de dados em list comprehension
+produtos = [
+    {'nome': 'p1', 'preco': 40, },
+    {'nome': 'p2', 'preco': 10, },
+    {'nome': 'p3', 'preco': 20, },
+]
+
+novos_produtos = [
+    {**produto, 'preco': produto['preco']*1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos]
+
+print(*novos_produtos, sep='\n')
