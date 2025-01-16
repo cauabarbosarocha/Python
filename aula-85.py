9fc3eeafd5ed817214ef73b7f277f65129b432a0
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-01-16 19:38:41 -0300

    Finalização da introdução de list comprehension
    Aprendi que é possível criar filtros

--- aula-85.py
+++ aula-85.py
@@ -3,6 +3,11 @@
 # a de iteráveis.
 # print(list(range(10)))
 
+import pprint
+
+def p(v):
+    pprint.pprint(v, sort_dicts=False, width=40)
+
 lista = []
 for numero in range(10):
     lista.append(numero)
@@ -22,6 +27,18 @@ produtos = [
 novos_produtos = [
     {**produto, 'preco': produto['preco']*1.05}
     if produto['preco'] > 20 else {**produto}
-    for produto in produtos]
+    for produto in produtos
+]
 
-print(*novos_produtos, sep='\n')
+# print(*novos_produtos, sep='\n')
+
+# lista = [n for n in range(10) if n < 5]
+# print(lista)
+
+novos_produtos = [
+    {**produto, 'preco': produto['preco']*1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+    if produto['preco'] >= 20 and produto['preco'] * 1.05 > 10
+]
+p(novos_produtos)
