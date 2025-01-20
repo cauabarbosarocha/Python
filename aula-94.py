40744e6ca06d1145e10ef4fb2214013f832b2801
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-01-20 19:16:40 -0300

    try e except para tratar exeções
    Aprendendo algumas formas de como se tratar uma exceção

--- aula-94.py
+++ aula-94.py
@@ -1,13 +1,17 @@
 # Try, except, else e finelly
 
 try:
-    numero_1 = int(input('Digite um número: '))
-    numero_2 = int(input('Digite um segundo número: '))
+    numero_1 = 90
+    numero_2 = 0
+    # print('Linha 1'[1000])
     divisao = numero_1 / numero_2
+    print('Linha 2')
 except ZeroDivisionError:
     print('[ERROR] Impossível dividir por 0.')
-except (ValueError, IndentationError):
-    print('[ERROR] TypeError ou IndexError.')
+except (TypeError, IndexError) as error:
+    print('TypeError ou IndexError.')
+    print('[ERROR]', error)
+    print('[CLASS]', error.__class__.__name__)
 # except NameError:
 #     print('[ERROR] Variável não definida.')
 except Exception:
