77d9f888a3c1e066c7741e879e4ccf0e19f294bf
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-01-21 19:52:47 -0300

    Aula-98 | Entendendo meus próprios módulos em python

---
+++ aula-98.py
@@ -0,0 +1,11 @@
+# Entendendo os seus próporios módulos em Python
+# O primeiro módulo executado chama-se __main__
+# Você pode importar outros módulos inteiros ou parte do módulo
+# O python conhece a pasta onde o __main__ está e as pastas
+# abaixo dele.
+# Ele não reconhece pastas e módulos acima do __main__
+# por padrão
+# O python conhece todos os módulos e pacotes presentes
+# nos caminhos de sys.path
+
+ 
