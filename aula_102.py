ed38f18820cf3319338ba2e58c91d855a4714bae
Cauã Barbosa <143126401+cauabarbosarocha@users.noreply.github.com>
2025-02-03 18:59:02 -0300

    Solução do exercício
    Acompanhei o professor para poder relembrar

--- aula_102.py
+++ aula_102.py
@@ -5,8 +5,12 @@ def soma(x, y):
 def multiplica(x, y):
     return x * y
 
-def cria_funcao(funcao, *args):
-    return funcao
+def cria_funcao(funcao, x):
+    def interna(y):
+        return funcao(x, y)
+    return interna 
 
 soma_cinco = cria_funcao(soma, 5)
 multiplica_dex = cria_funcao(multiplica, 10)
+print(soma_cinco(90))
+print(multiplica_dex(90))
